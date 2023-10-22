4e1a8d65ac43cf157b5d0f1e20fdaa726546c345
MarcinNowak94 <marcin94nowak@gmail.com>
2023-10-22 12:29:25 +0200

    Initializing repository with default project

---
+++ app/src/main/java/com/example/swim_01_02/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.swim_01_02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //https://www.tomaszx.pl/materialy/pum_lab1.pdf
+    //Lab 01 Excercise 02: Flashlight (screen)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
